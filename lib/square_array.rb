351ebe4e7956210f8d97479eaeea5b1c9e566237
Madeline Corman <madelinecorman@gmail.com>
2019-11-04 17:28:00 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,7 @@
 def square_array(array)
-  array = [1, 2, 3]
   counter = 0 
-  while array(counter) do
-    puts array(counter)**2
+  while square_array(counter) do
+    puts square_array(counter)**2
     counter += 1 
   end
 end
