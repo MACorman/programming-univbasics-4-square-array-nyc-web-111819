3b11165b24eccf32fd699e0b2102995c8d972a33
Madeline Corman <madelinecorman@gmail.com>
2019-11-04 18:07:16 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(numbers)
   result = Array.new(numbers.size)
   i = 0 
   while i < numbers.size do
-    result
+    result[i] = numbers[i]**2
     counter += 1 
   end
   square_array
